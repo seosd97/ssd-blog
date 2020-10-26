96381fb6e820f95f96253f64c145ef9fe6245697
seosd <seosd97@gmail.com>
2020-10-26 20:28:02 +0900

    remove default layout style

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `SSD's Blog`,
+    description: ``,
+    author: `seosd97`,
   },
   plugins: [
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default () => {
+  return (
+    <footer>
+      © 2020 SeungDuk Seo,
+      <span>
+        powered by <a href="https://www.gatsbyjs.com">Gatsby</a>
+      </span>
+    </footer>
+  )
+}
