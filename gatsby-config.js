f8e7bb6838b95c47c092655628d00fdf3b2b5de6
seosd <seosd97@gmail.com>
2020-12-19 20:36:51 +0900

    change title logo image

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
             resolve: 'gatsby-remark-autolink-headers',
             options: {
               elements: ['h2', 'h3'],
-            }
+            },
           },
           {
             resolve: 'gatsby-remark-prismjs',
@@ -51,13 +51,13 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'ssd-blog',
+        short_name: 'ssd-blog',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/logo-icon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
